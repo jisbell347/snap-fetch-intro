2d71ad8ba1a72434d20391d4e6e380f86ae0f46a
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-08 13:45:39 -0600

    Added basic html file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<script src="js/challenge.js"></script>
+		<title>Fetch Challenge</title>
+	</head>
+	<body>
+		<h1>Placeholders</h1>
+		<ul id="placeholders"></ul>
+	</body>
+</html>
